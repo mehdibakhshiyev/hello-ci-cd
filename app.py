7f8c26a8439852fc9b4a36f6646674e91d6729a4
mehdibakhshiyev <82920718+mehdibakhshiyev@users.noreply.github.com>
2026-02-19 00:17:24 -0500

    Updated app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello from Secured Python App on Port 8000 from SERVER200!"
+    return "Hello from Secured Python App on Port 8000 from SERVER300!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8000)
